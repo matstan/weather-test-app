e9b163b464c5131f6d57af6af7306687e9e1ab8a
matic <m@m.si>
2015-07-06 23:37:36 +0200

    Added skeleton for tests

---
+++ weather-test-data-parser/src/test/java/com/weather/test/app/data/parser/InitialTest.java
@@ -0,0 +1,19 @@
+package com.weather.test.app.data.parser;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Created by matic on 06/07/15.
+ */
+
+@RunWith(JUnit4.class)
+public class InitialTest {
+
+    @Test
+    public void initialTest() {
+        Assert.assertEquals(true, true);
+    }
+}
