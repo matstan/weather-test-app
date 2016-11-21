308b4f9a684ed7eb53451737cc594738f637ce63
Matic Standeker <matic.standeker@outfit7.com>
2016-11-21 12:51:43 +0100

    Resolve file uploading issue with using apache commons library

--- weather-test-gae/src/main/java/com/weather/test/app/gae/rest/FileResource.java
+++ weather-test-gae/src/main/java/com/weather/test/app/gae/rest/FileResource.java
@@ -1,16 +1,28 @@
 package com.weather.test.app.gae.rest;
 
-import com.weather.test.app.data.gcs.GcsFileService;
-import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
-import org.glassfish.jersey.media.multipart.FormDataParam;
+import static com.weather.test.app.gae.GaeAppSettings.FILE_UPLOAD_BUCKET_NAME;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 import javax.inject.Inject;
-import javax.ws.rs.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.StreamingOutput;
-import java.io.IOException;
-import java.io.InputStream;
+
+import org.apache.commons.fileupload.FileItemIterator;
+import org.apache.commons.fileupload.FileItemStream;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+import com.weather.test.app.data.gcs.GcsFileService;
 
 /**
  * Created by matic on 19.11.2016.
@@ -28,19 +40,31 @@ public class FileResource {
     @POST
     @Path("/upload")
     @Consumes({MediaType.MULTIPART_FORM_DATA})
-    public Response uploadFile(@FormDataParam("fileToUpload") InputStream fileInputStream,
-                               @FormDataParam("fileToUpload") FormDataContentDisposition fileDetail) throws IOException {
-
-        String fileName = fileDetail.getFileName();
-        String bucketName = "testBucket";
-        gcsFileService.writeFileToGCS(fileInputStream, fileName, bucketName);
+    public Response uploadFile(@Context HttpServletRequest req) throws IOException {
+        // Create a new file upload handler
+        ServletFileUpload upload = new ServletFileUpload();
+        try {
+            // Parse the request
+            FileItemIterator iter = upload.getItemIterator(req);
+            // iterate over all received files
+            while (iter.hasNext()) {
+                FileItemStream item = iter.next();
+                String name = item.getName();
+                InputStream stream = item.openStream();
+                // process only files (non-form fields
+                if (!item.isFormField()) {
+                    gcsFileService.writeFileToGCS(stream, name, FILE_UPLOAD_BUCKET_NAME);
+                }
+            }
+        } catch (FileUploadException e) {
+            e.printStackTrace();
+        }
 
         return Response.ok().build();
     }
 
     @GET
     @Path("/download")
-    //@Produces("text/plain")
     public Response downloadFile(@QueryParam("fileName") String fileName,
                                  @QueryParam("bucketName") String bucketName) throws IOException {
 
